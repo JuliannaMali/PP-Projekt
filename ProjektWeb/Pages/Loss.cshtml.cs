27e8e9fdc0dcdf86f933236b5b04f99a37d86beb
JuliannaMali <mali.julianna6@gmail.com>
2025-01-18 21:40:29 +0100

    wygląd walki + strona wygrana i przegrana

---
+++ ProjektWeb/Pages/Loss.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjektWeb.Pages
+{
+    public class LossModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ProjektWeb/Pages/Win.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjektWeb.Pages
+{
+    public class WinModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
